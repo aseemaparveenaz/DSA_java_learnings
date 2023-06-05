bf013e6c86aab68b4e7b280765ad7960d5c0b12e
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2023-06-05 22:54:28 +0530

    Create Pattern_4_rightTriRownum.java

---
+++ Pattern_programming/Pattern_4_rightTriRownum.java
@@ -0,0 +1,27 @@
+/*
+Example 1:
+
+Input: 5
+
+Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+
+*/
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+          for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        }
+    }
+}
