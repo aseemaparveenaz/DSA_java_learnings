6083a888c997c2981d467c14156d698391dd1b99
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2023-06-05 22:42:05 +0530

    Create Pattern_2_rightTri.java

---
+++ Pattern_programming/Pattern_2_rightTri.java
@@ -0,0 +1,28 @@
+/*
+Example 1:
+
+Input: 5
+
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+*/
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+        }
+    }
+}
