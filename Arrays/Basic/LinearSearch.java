ba9c6a96b4c881299e9696ffda4e2e2d2f6e879b
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2023-06-18 00:02:03 +0530

    Create LinearSearch.java

---
+++ Arrays/Basic/LinearSearch.java
@@ -0,0 +1,30 @@
+/*
+Problem Statement: Given an array, and an element num the task is to find if num is present in the given array or not. If present print the index of the element or print -1.
+
+Examples:
+
+Example 1:
+Input: arr[]= 1 2 3 4 5, num = 3
+Output: 2
+Explanation: 3 is present in the 2nd index
+
+Example 2:
+Input: arr[]= 5 4 3 2 1, num = 5
+Output: 0
+Explanation: 5 is present in the 0th index
+*/
+import java.util.*;
+public class Solution {
+    public static int linearSearch(int n, int num, int []arr){
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==num)
+        return i;
+    }
+    return -1;
+    }
+}
+//Time Complexity: O(n), where n is the length of the array.
+
+//Space Complexity: O(1)
+
