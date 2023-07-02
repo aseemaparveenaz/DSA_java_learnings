5c20aa14416db4b37fb11a7f4c5c8e6ba9d747ee
ASEEMA PARVEEN AZ <96627332+aseemaparveenaz@users.noreply.github.com>
2023-07-02 17:14:40 +0530

    Update Pattern_21.java

--- Pattern_programming/Pattern_21.java
+++ Pattern_programming/Pattern_21.java
@@ -8,3 +8,22 @@ Output:
 ****
 
 */
+class Solution {
+    void printSquare(int n) {
+        for(int i=1;i<=n;i++)
+        {
+           for(int j=1;j<=n;j++)
+           {
+               if((j==1) || (j==n) || (i==1) || (i==n))
+               {
+                   System.out.print("*");
+               }
+               else
+               {
+                  System.out.print(" ");
+               }
+           }
+         System.out.println();
+        }
+    }
+}
